4f85c9d4c7fd0b46537d4b3bfa24fb0577e934be
Tesitophobia <jreditssdatos@gmail.com>
2025-04-22 08:28:04 -0500

    update site URL in astro.config.mjs to point to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://github.com/Tesitophobia/PrisBits', // Cambia esto por la URL de tu repositorio
+  site: 'https://Tesitophobia.github.io/PrisBits', // Cambia esto por la URL de tu repositorio
   base: '/PrisBits/', // Cambia esto por el nombre de tu repositorio
   output: 'static', // Asegúrate de que sea un sitio estático
 });
